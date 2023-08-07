81226c66e2c88bb155323a967ac6e49246a91938
Julien Le Boucher <lou_berehc@tutanota.com>
2023-08-07 10:21:48 +0200

    Add paths and lightGBM params

--- classifiers/config.py
+++ classifiers/config.py
@@ -1,4 +1,14 @@
-FEATURE_PATH = "../pickle_files/features.pkl.gz"
-STRATIFIED_KFOLD = True
+import os
+
+# PATHS
+PROJECT_DIRECTORY = "/home/louberehc/OCR/projets/7_scoring_model"
+FEATURE_PATH = os.path.join(
+    PROJECT_DIRECTORY,
+    "pickle_files/features.pkl.gz"
+)
+
+# LightGBM model - configurations and hyperparameters
+NUM_THREADS = 4
 NUM_FOLDS = 5
-RANDOM_SEED = 784
+RANDOM_SEED = 737851
+
