58e4af3053151f8764adeb426616f1c77d1f568f
Julien Le Boucher <lou_berehc@tutanota.com>
2023-08-24 10:00:21 +0200

    Add Tracking_uri

--- hyperparameter_tuning/config.py
+++ hyperparameter_tuning/config.py
@@ -2,7 +2,8 @@ import os
 
 MAIN_DIR = "/home/louberehc/OCR/projets/7_scoring_model"
 DATA_PATH = os.path.join(MAIN_DIR, 'pickle_files/features.pkl.gz')
-HYPER_MLRUNS_PATH = os.path.join(MAIN_DIR, 'hyperparameter_tuning/mlruns')
+TRACKING_URI = os.path.join(MAIN_DIR, 'hyperparameter_tuning/mlruns')
+
 
 NUM_FOLDS = 5
 RANDOM_SEED = 137
