2176cbd3c564008d8ef05adae0199dc35f01daec
Julien Le Boucher <lou_berehc@tutanota.com>
2023-09-13 19:11:55 +0200

    Improve class description. Add an option to return None when there are to few people in the group.

--- dashboard/group.py
+++ dashboard/group.py
@@ -5,7 +5,14 @@ import matplotlib.pyplot as plt
 from numpy import isfinite
 
 class Group:
-    """ Group data structure."""
+    """ Group data structure that contains :
+    - a name.
+    - a description.
+    - a grouper which is a list of features to group by.
+    
+    Facilitates :
+    - data selection of a group of people similar to a certain customer.
+    - plotting those data. """
     
     def __init__(
         self, 
@@ -43,60 +50,67 @@ class Group:
         target = (
             target
             .astype("category")
-            .replace(0, 'customer without repayment risk')
-            .replace(1, 'customer with repayment risk')
+            .replace(0, 'without repayment risk')
+            .replace(1, 'with repayment risk')
         )
         # get group relative data 
         group_data = self.get_group_data(features_df, customer_id)
-        # Join the relevant feature of the group and the target,
-        # and make sure to keep only group data.
-        group_feature = group_data.loc[:, feature_name]
-        group_feature = (
-            pd.concat([group_feature, target], axis=1)
-            .loc[group_data.index, :]
-        )
-        # Get customer feature value if not null.
-        customer_val = group_feature.loc[customer_id, feature_name]
-        # plot 
-        fig, ax = plt.subplots(figsize=(8,4))
-        kdeplot(
-            data=group_feature,
-            x=feature_name,
-            hue='TARGET',
-            ax=ax,
-            legend=True,
-        )
-        # Custom 
-        title = (
-            f'Distributions per customer type of {feature_name}\n'
-            f'considering {self.description.lower()}\n'
-        )
-        ax.get_yaxis().set_visible(False)
-        _, y_max = ax.get_ylim()
-        x_min, x_max = ax.get_xlim()
-        if isfinite(customer_val):
-            # Draw an arrow pinpointing the customer value
-            ax.annotate(
-                'Customer value',
-                xy=(customer_val, 0),
-                xytext=(customer_val, 0.15*y_max),
-                arrowprops=dict(facecolor='gray', shrink=0.05),
-                ha='center',
-            )
+        if len(group_data) <= 3:
+            # There is no fig when it can not be meaningful
+            # because there are to few people
+            return None
         else:
-            ax.annotate(
-                'Unknown Customer value',
-                xy=((x_min + x_max)/2, 0.05*y_max),
-                ha='center',
+            # Join the relevant feature of the group and the target,
+            # and make sure to keep only group data.
+            group_feature = group_data.loc[:, feature_name]
+            group_feature = (
+                pd.concat([group_feature, target], axis=1)
+                .loc[group_data.index, :]
             )
-            
-        plt.title(title)
-        # plt.legend(
-        #     title='Customers:',
-        #     loc='best',
-        #     labels=['with repayment risk', 'without payment risk']
-        # )
-        return fig
+            # Rename the target columns for legend display.
+            group_feature = (
+                group_feature
+                .rename(
+                    columns={'TARGET': 'Customer type'},
+                )
+            )
+            # Get customer feature value.
+            customer_val = group_feature.loc[customer_id, feature_name]
+            # plot 
+            fig, ax = plt.subplots(figsize=(8,4))
+            kdeplot(
+                data=group_feature,
+                x=feature_name,
+                hue='Customer type',
+                ax=ax,
+                legend=True,
+            )
+            # Custom 
+            title = (
+                f'Distributions per customer type of {feature_name}\n'
+                f'considering {self.description.lower()}\n'
+            )
+            ax.get_yaxis().set_visible(False)
+            _, y_max = ax.get_ylim()
+            x_min, x_max = ax.get_xlim()
+            # Draw an arrow pinpointing the customer value, if there
+            # is one.
+            if isfinite(customer_val):
+                ax.annotate(
+                    'Customer value',
+                    xy=(customer_val, 0),
+                    xytext=(customer_val, 0.15*y_max),
+                    arrowprops=dict(facecolor='gray', shrink=0.05),
+                    ha='center',
+                )
+            else:
+                ax.annotate(
+                    'Unknown Customer value',
+                    xy=((x_min + x_max)/2, 0.05*y_max),
+                    ha='center',
+                )
+            plt.title(title)
+            return fig
 
         
         
