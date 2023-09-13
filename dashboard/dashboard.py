f39d33d48f10112595a913f53002f8a69cd6e94f
Julien Le Boucher <lou_berehc@tutanota.com>
2023-09-13 19:11:02 +0200

    Change and add API requests. Add model output mapping to the bar score. Add an expander for debugging with technical information about model and customer. Improve plotting in the comparison tab.

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,7 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import requests
-# import json
+import io
+import json
 from urllib.parse import urljoin
 import time
 from group import Group
@@ -10,10 +11,10 @@ import random
 import base64
 import visual_elem
 
+DEBUG = True
+
+#
 API_ROOT = "http://localhost:8435/"
-
-
-
 
 ICON_PATH = "./images/tab_icon.png"
 APP_NAME = "Credit attribution explorer"
@@ -26,10 +27,10 @@ st.set_page_config(
     initial_sidebar_state="auto",
 )
 
-DATA_URI = (
-    '/home/louberehc/OCR/projets/7_scoring_model/'
-    'pickle_files/reduced_data.pkl'
-)
+# DATA_URI = (
+#     '/home/louberehc/OCR/projets/7_scoring_model/'
+#     'pickle_files/reduced_data.pkl'
+# )
 
 def render_svg(svg):
     """Renders the given svg string."""
@@ -40,12 +41,59 @@ def render_svg(svg):
 @st.cache_data
 def get_data():
     """
-    Get the data about of all customers and cache it.
-    Convert values to float64 to avoid later problems.
+    Request data features about all customers to the API to use for 
+    comparison, return it as a dataframe and cache it.
+    
+    There, all customers are used because I am only using a very light
+    sample of data to avoid cost while deploying the API.
+    It could be interesting to sample in the function if the data
+    becomes larger.
     """
-    df = pd.read_pickle(DATA_URI).astype("float64")
-    target = df.pop('TARGET')
-    return df, target
+    request_url = urljoin(API_ROOT, 'data')
+    r = requests.get(request_url)
+    # Retrieve the json string because I found no way to convert the r.json()
+    # to a dataframe directly.
+    t = r.text
+    return pd.read_json(io.StringIO(t))
+
+@st.cache_data
+def get_target():
+    """
+    Request data features about all customers to the API to use for 
+    comparison, return it as a DataFrame and cache it.
+    
+    There, all customers are used because I am only using a very light
+    sample of data to avoid cost while deploying the API.
+    It could be interesting to sample in the function if the data
+    becomes larger.
+    """
+    request_url = urljoin(API_ROOT, 'target')
+    r = requests.get(request_url)
+    # Retrieve the json string because I found no way to convert the r.json()
+    # to a dataframe directly.
+    t = r.text
+    return pd.read_json(io.StringIO(t))
+
+
+@st.cache_data
+def get_customer_info(customer_id):
+    """ Request a model inference via the API and return the response."""
+    request_url = urljoin(API_ROOT, f'prediction/{customer_id}')
+    r = requests.get(request_url)
+    return r
+
+
+@st.cache_data
+def get_model_info():
+    """ Request model information and return the response."""
+    request_url = urljoin(API_ROOT, 'model_info')
+    r = requests.get(request_url)
+    return r
+
+
+def convert_requests_response_to_dict(response):
+    return json.loads(response.text)
+
 
 @st.cache_data
 def instantiate_groups():
@@ -63,20 +111,6 @@ def instantiate_groups():
         ),
     ]
     return group_list
-        
-
-@st.cache_data
-def get_customer_score_fake(customer_data):
-    time.sleep(3)
-    return random.randint(0, 100), 50
-
-
-@st.cache_data
-def get_customer_score(customer_id):
-    request_url = urljoin(API_ROOT, f'prediction/{customer_id}')
-    r = requests.get(request_url)
-    return r
-    
 
 
 def ChangeWidgetFontSize(wgt_txt, wch_font_size = '12px'):
@@ -87,17 +121,39 @@ def ChangeWidgetFontSize(wgt_txt, wch_font_size = '12px'):
     htmlstr = htmlstr.replace('|wgt_txt|', "'" + wgt_txt + "'")
     components.html(f"{htmlstr}", height=0, width=0)
     
+
+def map_model_output_to_bar_position(score, decision_threshold):
+    """ Map arbitrarily the model proba prediction in [0, 1] to its 
+    position on a score bar which goes from 0 to 100 having a decision
+    boundary representation in the middle when the model decision threshold
+    is not necessarily at 0.5.
     
+    It could be done the smart way observing scores of true and false positive
+    in order to identify where the model has a nice certainty (extremes)
+    and where it is closer to the boundary decision.
+    """
+    # Arbitrary chosen values
+    left_uncertain = model_threshold - 0.03 
+    right_uncertain = model_threshold + 0.03 
+    delta_uncertain = right_uncertain - left_uncertain
+    if score < left_uncertain:
+        return (score / left_uncertain * 40)
+    elif (score > left_uncertain) and (score < right_uncertain):
+        return (40 + ((score-left_uncertain) / delta_uncertain)*20)
+    else:
+        return (60 + (score-right_uncertain)/(1-right_uncertain)*40)
 
 
+###### Global requests
+features = get_data()
+target = get_target()
+model_info = convert_requests_response_to_dict(get_model_info())
 
+# Extract some useful values
+model_threshold = model_info['decision_threshold']
+valid_customer_ids = list(features.index)
 
-
-
-###### Global variable definitions
-df, target = get_data()
-valid_customer_ids = list(df.index)
-fts_by_model_importance = df.columns # TODO: change with model global interp.
+fts_by_model_importance = features.columns # TODO: change with model global interp.
 
 with st.sidebar:
     # Selection of the customer
@@ -111,40 +167,52 @@ with st.sidebar:
     else:
         # Get customer data and format it.
         customer_data = (
-            df
+            features
             .loc[customer_id, :]
             .T
         )
         customer_data.index.name = 'Feature name'
         customer_data.name = 'Value'
-        # Eventually use a pd styler.
         
-        with st.spinner('Wait for the model decision...'):
-            # Send request to the model API 
-            # customer_score, model_threshold = get_customer_score_fake(customer_data)
-            r = get_customer_score(customer_id)
-            st.json(r.json())
+        # Send request to the model API to get customer prediction
+        # as a json object
+        customer_info = convert_requests_response_to_dict(
+            get_customer_info(customer_id)
+        )
+            
         # Print the model decision
         st.divider()
         st.write("### Model decision")
+        if customer_info['class'] == 'no_risk':
+            render_svg(visual_elem.ACCEPTED_SVG)
+        else:
+            render_svg(visual_elem.REJECTED_SVG)
+            
+        # Print gauge to tell if close from the decision boundary
+        st.divider()
+        st.write("### Model perception")
+        score_on_bar = map_model_output_to_bar_position(
+            score=customer_info['proba_risk_class'],
+            decision_threshold=model_threshold
+        )
+        st.markdown(
+            visual_elem.draw_score_bar(score_on_bar),
+            unsafe_allow_html=True
+        )
         
-        # if customer_score < model_threshold:
-        #     render_svg(visual_elem.ACCEPTED_SVG)
-        # else:
-        #     render_svg(visual_elem.REJECTED_SVG)
-        # # Print gauge to tell if close from the decision boundary
-        # st.divider()
-        # st.write("### Model perception")
-        # st.markdown(
-        #     visual_elem.draw_score_bar(customer_score),
-        #     unsafe_allow_html=True
-        # )
-        
-        
+        if DEBUG:
+            st.divider()
+            with st.expander('Technical information'):
+                st.write("Model information:")
+                st.write(model_info)
+                st.write("Customer information:")
+                st.write(customer_info)
+
+### Dashboard central part :
 # Application title
 render_svg(visual_elem.TITLE_SVG)
         
-# start tabs       
+# Tabs creation       
 tab1, tab2, tab3 = st.tabs(
     [
         "Explore customer's data",
@@ -209,7 +277,7 @@ with tab3:
         st.write('')
     else:
         # Group definitions once and for all thanks to a function with 
-        # a cache_data decorator (avoid reload and duplication)
+        # a cache_data decorator
         groups = instantiate_groups()
         # Group selection
         sel_group_name = st.radio(
@@ -218,7 +286,8 @@ with tab3:
                     captions=[g.description for g in groups]
                 )
         ChangeWidgetFontSize('Who to compare too?', '18px')
-            
+        
+        ChangeWidgetFontSize('Univariate analysis', '20px')
         with st.expander('Univariate analysis'):
             # Feature selection
             sel_ft = st.selectbox(
@@ -226,18 +295,25 @@ with tab3:
                 options=fts_by_model_importance,
             )
             ChangeWidgetFontSize('Which feature to compare?', '18px')
-            # Get the Group class and use it to plot.
+            # Get the select group and use it to plot.
             sel_group = [g for g in groups if g.name == sel_group_name][0]
             fig = sel_group.plot_feature_kde_with_client_value(
-                features_df=df,
+                features_df=features,
                 target=target,
                 customer_id=customer_id,
                 feature_name=sel_ft,
             )
-            st.pyplot(fig)
+            if fig is not None:
+                st.pyplot(fig)
+            else: 
+                st.write(
+                    "There are to few people in the group to "
+                    "plot a meaningful visualization.")
             
+            st.markdown(visual_elem.plot_note)
+            
+        ChangeWidgetFontSize('Bi-variate analysis', '20px')
         with st.expander('Bi-variate analysis'):
-            ChangeWidgetFontSize('Bi-variate analysis', '18px')
             col1, _, col2 = st.columns([1, 0.2, 1])
             with col1:
                 ft_x = st.selectbox(
